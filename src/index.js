a500edc83d2cebf6ecd71ab2fc886eef2eb257cc
Kevin Sjöberg <kev.sjoberg@gmail.com>
2017-12-09 23:08:29 +0100

    Setup Redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
